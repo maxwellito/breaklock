81fda3a8250cabfc02a8520fba2644e86f9fec6e
maxwellito <duluc.maxime@gmail.com>
2017-04-17 10:46:54 +0100

    Update fetch in service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'breaklock_003';
+var CACHE_NAME = 'breaklock_004';
 var filesToCache = [
   './',
   './app.css',
@@ -33,4 +33,44 @@ self.addEventListener('activate', function(event) {
       );
     })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
